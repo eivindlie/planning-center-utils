0490c5edd1f8762cd15e753350fdef537e985d07
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2022-08-13 16:49:40 +0200

    Fix linter issue (for real this time)

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -1,14 +1,14 @@
 import { useEffect, useMemo, useState } from "react";
 
 export const useMediaQuery = (query: string) => {
-  const mediaQueryList = useMemo(() => matchMedia(query), []);
+  const mediaQueryList = useMemo(() => matchMedia(query), [query]);
   const [matches, setMatches] = useState(mediaQueryList.matches);
 
   useEffect(() => {
     const handler = () => setMatches(mediaQueryList.matches);
     mediaQueryList.addEventListener("change", handler);
     return () => mediaQueryList.removeEventListener("change", handler);
-  }, []);
+  }, [mediaQueryList]);
 
   return matches;
 };
