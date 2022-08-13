0adb62454ca9b5b12c218e5b799c2098cf501135
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2022-08-13 12:48:23 +0200

    Add AuthUserContext

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,9 @@ import { Routes } from "routes";
 import { COLORS } from "style/variables";
 import { NavMenu } from "components/navigation/NavMenu";
 import { PageHeader } from "./navigation/PageHeader";
+import { AuthUserContext } from "contexts/AuthUserContext";
+import { useState } from "react";
+import { getAuth, User } from "firebase/auth";
 
 const useStyles = createUseStyles({
   app: {
@@ -24,18 +27,26 @@ const useStyles = createUseStyles({
 });
 
 const App = () => {
+  const [authUser, setAuthUser] = useState(getAuth().currentUser);
+
+  getAuth().onAuthStateChanged((authUser) => {
+    setAuthUser(authUser);
+  });
+
   const classes = useStyles();
 
   return (
-    <div className={classes.app}>
-      <PageHeader />
-      <div className={classes.wrapper}>
-        <NavMenu />
-        <main className={classes.content}>
-          <Routes />
-        </main>
+    <AuthUserContext.Provider value={authUser}>
+      <div className={classes.app}>
+        <PageHeader />
+        <div className={classes.wrapper}>
+          <NavMenu />
+          <main className={classes.content}>
+            <Routes />
+          </main>
+        </div>
       </div>
-    </div>
+    </AuthUserContext.Provider>
   );
 };
 
---
+++ src/contexts/AuthUserContext.ts
@@ -0,0 +1,7 @@
+import { getAuth, User } from "@firebase/auth";
+import { createContext, useContext } from "react";
+
+export const AuthUserContext = createContext<User | null>(null);
+export const useAuthUser = () => {
+  return useContext(AuthUserContext);
+};
