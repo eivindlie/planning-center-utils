7efd3c840c9495cce0b22a53019e10e4aff865ad
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2022-09-20 08:25:03 +0200

    Set blockouts as default route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,12 @@
 import { Plans } from "components/plans/Plans";
-import { Route, Routes as BrowserRoutes } from "react-router-dom";
+import { Navigate, Route, Routes as BrowserRoutes } from "react-router-dom";
 import { Teams } from "./components";
 import { Blockouts } from "./components/blockout/Blockouts";
 
 export const Routes = () => {
   return (
     <BrowserRoutes>
+      <Route path="/" element={<Navigate to="/blockouts" />} />
       <Route path={"/teams"} element={<Teams />} />
       <Route path={"/blockouts"} element={<Blockouts />} />
       <Route path={"/plans"} element={<Plans />} />
