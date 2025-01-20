5c4ae7cfa0d74f07db6d323b1bcf3c1f5ac297cd
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2025-01-20 18:41:52 +0100

    Delete garbage

--- src/hooks/useTeamsWithBlockouts.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-import { ITeamWithBlockouts } from "types";
-
-export const useTeamsWithBlockouts = () => {
-    const [teamsWithBlockouts, setTeamsWithBlockouts] = useState<ITeamWithBlockouts[]>([]);
-
-    
-};
