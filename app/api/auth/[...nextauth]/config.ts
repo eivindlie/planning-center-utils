7f3b8f100cf964cd6f0514d9b47a3ebe3263ecbb
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2025-01-20 20:12:16 +0100

    Add Google auth

--- app/api/auth/[...nextauth]/config.ts
+++ app/api/auth/[...nextauth]/config.ts
@@ -1,7 +1,19 @@
+import prisma from "@/prisma/client";
 import { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: AuthOptions = {
+    callbacks: {
+        signIn: async ({user, account, profile}) => {
+            if (account?.provider === "google") {
+                const userFromDb = await prisma.user.findFirst({where: {email: profile?.email!}});
+                if (!userFromDb) {
+                    await prisma.user.create({data: {email: profile?.email!, name: profile?.name!}});
+                }
+            }
+            return true;
+        },
+    },
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
