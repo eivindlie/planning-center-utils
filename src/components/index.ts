dd2abeda0e0d9adf69302ec3bcd8844fa3987a29
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2025-01-20 17:01:40 +0100

    Clean slate

--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { Spinner } from "./_basis/Spinner";
-export { Button } from "./_basis/Button";
-
-export { People } from "./people/People";
-export { Teams } from "./people/Teams";
--- src/contexts/AuthUserContext.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "@firebase/auth";
-import { createContext, useContext } from "react";
-
-export const AuthUserContext = createContext<User | null>(null);
-export const useAuthUser = () => {
-  return useContext(AuthUserContext);
-};
