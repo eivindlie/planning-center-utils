0f2d06448423cdca371136194502b251839d06b8
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2025-01-22 20:51:24 +0100

    Make header link to start

--- app/(authenticated)/Header.tsx
+++ app/(authenticated)/Header.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import { ExportStatus } from "./ExportStatus";
 import styles from "./Header.module.css";
 
 export const Header = () => {
   return (
     <div className={styles.header}>
-        <h1>Planning Center Utils</h1>
+      <h1>
+        <Link href="/">Planning Center Utils</Link>
+      </h1>
       <ExportStatus />
     </div>
   );
