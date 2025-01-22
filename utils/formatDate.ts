a312a5f9533232274de2ca0daffe4b829a362a20
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2025-01-22 18:58:29 +0100

    Use tz-aware datetime formatting

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,19 +1,22 @@
 export const formatDate = (date: Date): string => {
-    const yyyy = date.getFullYear();
-    let mm = (date.getMonth() + 1).toString(); // Months start at 0!
-    let dd = date.getDate().toString();
-    
-    if (dd.length < 2) dd = '0' + dd;
-    if (mm.length < 2) mm = '0' + mm;
-    
-    const formattedDate = dd + '.' + mm + '.' + yyyy;
-    return formattedDate;
+  const yyyy = date.getFullYear();
+  let mm = (date.getMonth() + 1).toString(); // Months start at 0!
+  let dd = date.getDate().toString();
+
+  if (dd.length < 2) dd = "0" + dd;
+  if (mm.length < 2) mm = "0" + mm;
+
+  const formattedDate = dd + "." + mm + "." + yyyy;
+  return formattedDate;
 };
 
-const formatTime = (date: Date): string => {
-    return date.toLocaleTimeString('nb-NO', { hour: '2-digit', minute: '2-digit' });
-}
-
 export const formatDateTime = (date: Date): string => {
-    return `${formatDate(date)} ${formatTime(date)}`;
-}
+  return Intl.DateTimeFormat("nb-NO", {
+    timeZone: "Europe/Oslo",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(date);
+};
